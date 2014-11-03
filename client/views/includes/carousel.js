7c1f9413e6baabba041c1c251e70bfe3b167b1c7
Gabe Perez <perez283@gmail.com>
2014-11-03 14:48:07 -0500

    Changes default color, adds new logos, and updates carousel information

--- client/views/includes/carousel.js
+++ client/views/includes/carousel.js
@@ -4,11 +4,14 @@ Template.carousel.events = {
     setTimeout( function() {
       $('#search').focus();
     }, 500);
+  },
+  'click .carousel-new-petition' : function () {
+    window.location.href="/petitions/create";
   }
 }
 
 Template.carousel.rendered = function onCarouselRendered() {
-  var stock_images = ['carousel_1.png', 'carousel_2.png', 'carousel_3.png'];
+  var stock_images = ['carousel_1.png', 'carousel_2.png', 'carousel_3.png', 'carousel_4.png', 'carousel_5.png', 'carousel_6.png'];
   var random = stock_images[Math.floor(Math.random() * stock_images.length)];
   $('.carousel').css('background-image', 'url(' + random + ')');
   return;
